c2123084cb16d187fc63f66b24ca6400b509d986
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2022-11-10 17:47:41 -0500

    status 200

--- controllers/home.controllers.ts
+++ controllers/home.controllers.ts
@@ -1,15 +1,6 @@
 import { Request, Response } from "express";
 
 export const getHome = async (req: Request, res: Response) => {
-  try {
-    res.json({
-      ok: true,
-      msg: "Todo OK",
-    });
-  } catch (error) {
-    res.status(500).json({
-      msg: "Hable con el administrador",
-      error,
-    });
-  }
+  res.status(200);
+  res.send({ ok: "Todo OK" });
 };
