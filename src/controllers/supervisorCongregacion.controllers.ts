49c2af0b45ade2719d3422e9e6edff90620313ba
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2024-04-10 17:15:33 -0500

    Avances feligreses del supervisor

--- src/controllers/supervisorCongregacion.controllers.ts
+++ src/controllers/supervisorCongregacion.controllers.ts
@@ -1,27 +1,73 @@
 import { Request, Response } from "express";
 import db from "../database/connection";
+import Usuario from "../models/usuario.model";
+import Pais from "../models/pais.model";
+import Congregacion from "../models/congregacion.model";
+import Campo from "../models/campo.model";
 
 export const getFeligreses = async (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    const [feligres, metadata] =
-      await db.query(`SELECT count(u.id), u.id, u.primerNombre, u.segundoNombre, u.primerApellido, 
-                        u.segundoApellido, u.email, u.fechaNacimiento, u.numeroCelular, p.pais, sc.obrero_id, c.congregacion,  sc.obrero_id, u.estado FROM  
-                        usuario u 
-                        INNER JOIN usuarioCongregacion uc 
-                        ON u.id = uc.usuario_id 
-                        INNER JOIN pais p 
-                        ON uc.pais_id = p.id
-                        INNER JOIN congregacion c 
-                        ON uc.congregacion_id = c.id                                         
-                        INNER JOIN supervisorCongregacion sc 
-                        ON sc.pais_id = p.id 
-                        WHERE sc.obrero_id = ${id} AND u.estado = 1
-                        ORDER BY u.primerNombre;`);
 
+  //TODO queda pendiente por terminar.
+
+  try {
+    // Validar si idusuario es un número válido
+    if (!id) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El ID del usuario no es válido.",
+      });
+    }
+
+    // Buscar la congregación del obrero encargado con el ID proporcionado
+    const pais = await Pais.findOne({
+      where: { idObreroEncargado: id },
+    });
+
+    if (!pais) {
+      return res.status(404).json({
+        message: "El obrero no tiene congregación a cargo",
+      });
+    }
+
+    // Obtener todos los usuarios asociados a esta congregación
+    const { count, rows } = await Usuario.findAndCountAll({
+      attributes: [
+        "id",
+        "primerNombre",
+        "segundoNombre",
+        "primerApellido",
+        "segundoApellido",
+        "apodo",
+        "fechaNacimiento",
+        "email",
+        "numeroCelular",
+        "estado",
+      ],
+      include: [
+        {
+          model: Pais,
+          as: "usuarioCongregacionPais",
+          attributes: ["pais"],
+          where: { id: id },
+        },
+        {
+          model: Congregacion,
+          as: "usuarioCongregacionCongregacion",
+
+          attributes: ["congregacion"],
+        },
+        {
+          model: Campo,
+          as: "usuarioCongregacionCampo",
+          attributes: ["campo"],
+        },
+      ],
+    });
     res.json({
       ok: true,
-      feligres,
+      feligres: rows,
+      totalUsuarios: count,
     });
   } catch (error) {
     res.status(500).json({
